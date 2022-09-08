fd38c1e9a80b5f1209d5b7489c24aa442e99c26e
juzorh <juzor94@gmaijuzor94@gmail.com>
2022-09-08 05:19:52 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+  * main - Programs entry point
+  *
+  * Returns: 0 - successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
