df67174d7af76148f2dfe66cf5126eb94ab59576
juzorh <juzor94@gmaijuzor94@gmail.com>
2022-09-15 07:48:39 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Prints '_' n times
+ * Prototype: void print_line(int n)
+ * @n: number of times to print _
+ * Return: null/void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (n--)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
