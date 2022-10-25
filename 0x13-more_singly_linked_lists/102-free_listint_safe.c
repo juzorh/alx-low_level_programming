1bfde39f989b80e1814f2f38b5492a8f3d049180
Joseph Uzor <49317280+juzorh@users.noreply.github.com>
2022-10-25 19:10:24 +0100

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -13,39 +13,39 @@ size_t free_listint_safe(listint_t **h);
  */
 size_t looped_listint_count(listint_t *head)
 {
-	listint_t *tortoise, *hare;
+	listint_t *sl, *fa;
 	size_t nodes = 1;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
 
-	tortoise = head->next;
-	hare = (head->next)->next;
+	sl = head->next;
+	fa = (head->next)->next;
 
-	while (hare)
+	while (fa)
 	{
-		if (tortoise == hare)
+		if (sl == fa)
 		{
-			tortoise = head;
-			while (tortoise != hare)
+			sl = head;
+			while (sl != fa)
 			{
 				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
+				sl = sl->next;
+				fa = fa->next;
 			}
 
-			tortoise = tortoise->next;
-			while (tortoise != hare)
+			sl = sl->next;
+			while (sl != fa)
 			{
 				nodes++;
-				tortoise = tortoise->next;
+				sl = sl->next;
 			}
 
 			return (nodes);
 		}
 
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
+		sl = sl->next;
+		fa = (fa->next)->next;
 	}
 
 	return (0);
